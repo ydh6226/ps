2062b7230ce8e7a6c5456012de2c8f5c10803718
ydh6226 <ydh6226@naver.com>
2020-12-29 08:22:01 +0900

    samsung12100

---
+++ 삼성/백준12100.cpp
@@ -0,0 +1,87 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int answer;
+int n;
+
+void rotate(vector<vector<int>>& board) {
+	vector<vector<int>> temp(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = n - 1; j >= 0; j--) {
+			temp[i][n - j - 1] = board[j][i];
+		}
+	}
+	
+	board = temp;
+}
+
+vector<vector<int>> up(vector<vector<int>> board) {
+	for (int i = 0; i < n; i++) {
+		vector<int> v;
+		bool used = false;
+
+		for (int j = 0; j < n; j++) {
+			if (board[j][i] == 0)
+				continue;
+
+			if (v.size() == 0)
+				v.push_back(board[j][i]);
+			else {
+				if (used == false && v.back() == board[j][i]) {
+					v.back() *= 2;
+					used = true;
+				}
+				else {
+					v.push_back(board[j][i]);
+					used = false;
+				}
+			}
+		}
+
+		for (int j = 0; j < v.size(); j++) {
+			board[j][i] = v[j];
+		}
+		for (int j = v.size(); j < n; j++) {
+			board[j][i] = 0;
+		}
+	}
+
+	cout << endl;
+	return board;
+}
+
+void dfs(vector<vector<int>> board, int count) {
+	if (count == 5) {
+		int m = 0;
+		for (int i = 0; i < n; i++) {
+			m = max(m, *max_element(board[i].begin(), board[i].end()));
+		}
+		
+		answer = max(answer, m);
+		return;
+	}
+
+
+	for (int i = 0; i < 4; i++) {
+		dfs(up(board), count + 1);
+		rotate(board);
+	}
+}
+
+
+int main() {
+	scanf("%d", &n);
+	vector<vector<int>> board(n, vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			int x;
+			scanf("%d", &x);
+			board[i][j] = x;
+		}
+	}
+	dfs(board, 0);
+	cout << answer << endl;
+}
---
+++ 삼성/삼성3190.cpp
@@ -0,0 +1,73 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+//0 길, 1 사과, 2 뱀
+int game[102][102];
+
+int main() {
+	int n, k, l;
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i < k; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+
+		game[a][b] = 1;
+	}
+
+	vector < pair<int, char>> dir;
+	scanf("%d", &l);
+	for (int i = 0; i < l; i++) {
+		int a;
+		char b;
+		scanf("%d %c", &a, &b);
+		dir.push_back({ a, b });
+	}
+
+	int d = 1;//0123 북동남서
+	int dx[] = { -1,0,1,0 };
+	int dy[] = { 0,1,0,-1 };
+	vector<pair<int, int>> snake = { {1,1} };
+
+	game[1][1] = 2;
+	for (int i = 1; ; i++) {
+		int hx, hy, tx, ty, nx, ny;
+		hx = snake.back().first;
+		hy = snake.back().second;
+		tx = snake.front().first;
+		ty = snake.front().second;
+
+		nx = hx + dx[d];
+		ny = hy + dy[d];
+
+		if (nx == 0 || nx == n + 1 || ny == 0 || ny == n + 1 || game[nx][ny] == 2) {
+			cout << i << endl;
+			break;
+		}
+
+		snake.push_back({ nx, ny });
+
+		if (game[nx][ny] != 1) {
+			game[tx][ty] = 0;
+			snake.erase(snake.begin());
+		}
+
+		game[nx][ny] = 2;
+		hx = nx;
+		hy = ny;
+
+		if (dir.empty() == false && i == dir.front().first) {
+			if (dir.front().second == 'D') {
+				d = (d + 1) % 4;
+			}
+			else {
+				if (d == 0)
+					d = 3;
+				else
+					d = d - 1;
+			}
+			dir.erase(dir.begin());
+		}
+	}
+}

e342ddcb85049a313d6569348812ae8c676aa529
ydh6226 <ydh6226@naver.com>
2021-01-17 16:07:48 +0900

    baekjoon11726

---
+++ 삼성/백준11726.cpp
@@ -0,0 +1,18 @@
+﻿#include<bits/stdc++.h>
+
+#define SIZE 1002
+
+int n;
+int dp[SIZE];
+
+int main() {
+	scanf("%d", &n);
+
+	dp[0] = 1;
+	dp[1] = 2;
+
+	for (int i = 2; i < n; i++) {
+		dp[i] = ( dp[i-2] + dp[i-1]) % 10007;
+	}
+	printf("%d", dp[n - 1]);
+}

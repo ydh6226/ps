107760166581a856dd6b19d17159983d2aa4f073
ydh6226 <ydh6226@naver.com>
2021-01-23 03:05:11 +0900

    2020kakaoblind_first_1

---
+++ 카카오/2020카카오블라인드_1.cpp
@@ -0,0 +1,38 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int solution(string s) {
+    int size = s.size();
+    int answer = size;
+
+    for (int i = 1; i <= size/2; i++) {
+        string prev = s.substr(0, i);
+        int count = 1;
+        int len = 0;
+
+        int index = i;
+        while (index < size) {
+            string cur = s.substr(index, i);
+
+            if (prev == cur) {
+                count++;
+            }
+            else {
+                len += i;
+                if (count != 1)
+                    len += to_string(count).size();
+
+                prev = cur;
+                count = 1;
+            }
+            index += i;
+        }
+        if (count == 1)
+            len += prev.size();
+        else
+            len += i + to_string(count).size();
+        answer = min(answer, len);
+    }
+    return answer;
+}

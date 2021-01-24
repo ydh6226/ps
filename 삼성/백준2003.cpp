65d409edbc077e07bb4a3dcfb68a615dab561069
ydh6226 <ydh6226@naver.com>
2021-01-25 01:50:59 +0900

    baekjoon2003

---
+++ 삼성/백준2003.cpp
@@ -0,0 +1,28 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+int board[10002];
+
+int main() {
+	int n, m, answer = 0;
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &board[i]);
+	}
+
+	int l = 0, r = 0;
+	int sum = board[l];
+	while (l<n && r<n) {
+		if (sum == m)
+			answer++;
+
+		if (sum < m)
+			sum += board[++r];
+		else
+			sum -= board[l++];
+	}
+	printf("%d", answer);
+}

48785522f0aa067d52af28d5952ef707178a5b56
ydh6226 <ydh6226@naver.com>
2021-01-27 01:37:42 +0900

    20201kakao_blind_1

---
+++ 카카오/2021카카오블라인드_1.cpp
@@ -0,0 +1,46 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+string solution(string new_id) {
+    string answer = "";
+
+    transform(new_id.begin(), new_id.end(), new_id.begin(), ::tolower);
+
+    string tmp = "";
+    int count = 0;
+
+    for (int i = 0; i < new_id.size(); i++) {
+        char c = new_id[i];
+        if (isdigit(c) || islower(c) || isupper(c) || c == '-' || c == '_' || c == '.') {
+            if (c == '.')
+                count++;
+            else
+                count = 0;
+
+            if (count < 2)
+                tmp += c;
+        }
+    }
+
+    if (tmp.front() == '.')
+        tmp.erase(0, 1);
+
+    if (tmp.back() == '.')
+        tmp.erase(tmp.size() - 1, 1);
+
+    if (tmp.empty())
+        tmp += 'a';
+
+    if (tmp.size() >= 16)
+        tmp = tmp.substr(0, 15);
+
+    if (tmp.back() == '.')
+        tmp.erase(tmp.size() - 1, 1);
+
+    while (tmp.size() <= 2) {
+        tmp += tmp.back();
+    }
+
+    return tmp;
+}

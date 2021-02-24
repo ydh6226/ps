9fed8b05e8b112c3b5cc8e29ffa60a4085c99a0b
ydh6226 <ydh6226@naver.com>
2021-02-24 23:45:11 +0900

    kakao_blind_third_5

---
+++ 카카오/2018카카오블라인드_3차_5번.cpp
@@ -0,0 +1,49 @@
+﻿#include<bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<string> words) {
+    int answer = 0;
+
+    words.push_back("");
+    sort(words.begin(), words.end());
+    words.push_back("");
+
+    for (int i = 1; i < words.size() - 1; i++) {
+        int j, size;
+        int count1 = 0, count2 = 0;
+
+        string before = words[i - 1];
+        string cur = words[i];
+        string next = words[i + 1];
+
+        size = min(before.size(), cur.size());
+        for (j = 0; j < size; j++) {
+            if (before[j] != cur[j]) {
+                break;
+            }
+            count1++;
+        }
+        if (j != cur.size()) {
+            count1++;
+        }
+
+        size = min(cur.size(), next.size());
+        for (j = 0; j < size; j++) {
+            if (cur[j] != next[j]) {
+                break;
+            }
+            count2++;
+        }
+        if (j != cur.size()) {
+            count2++;
+        }
+
+        answer += std::max(count1, count2);
+    }
+    return answer;
+}
+
+int main() {
+    solution({ "word", "war", "warrior", "world" });
+}

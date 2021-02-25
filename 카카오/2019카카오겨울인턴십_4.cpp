810e5cf664fb55d6be6a6c65f45038c45ace06bf
ydh6226 <ydh6226@naver.com>
2021-02-25 17:09:49 +0900

    2019kakao_winter_intern_4

---
+++ 카카오/2019카카오겨울인턴십_4.cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+#include<unordered_map>
+
+using namespace std;
+using ll = long long;
+
+unordered_map<ll, ll> uMap;
+
+ll getRoom(ll num) {
+    if (uMap[num] == 0) {
+        uMap[num] = num + 1;
+        return num;
+    }
+    return uMap[num] = getRoom(uMap[num]);
+}
+
+vector<ll> solution(ll k, vector<ll> room_number) {
+    vector<ll> answer;
+    for (ll num : room_number) {
+        answer.push_back(getRoom(num));
+    }
+    return answer;
+}

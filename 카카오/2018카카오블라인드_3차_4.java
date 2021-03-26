7471ee691d5f693209a4e70d1edd2c945baed57f
ydh6226 <ydh6226@naver.com>
2021-03-26 21:50:48 +0900

    2018kakao_blind_third_4

---
+++ 카카오/2018카카오블라인드_3차_4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.solution("CC#BCC#BCC#BCC#B", new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"});
+    }
+}

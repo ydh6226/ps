3416703e38f45d707921f922a0b815c279c779e5
ydh6226 <ydh6226@naver.com>
2021-04-17 20:07:50 +0900

    '백준9095'

---
+++ 백준/백준9095 1,2,3 더하기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 4; i < 11; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            bw.write(dp[x] + "\n");
+        }
+        bw.flush();
+    }
+}
